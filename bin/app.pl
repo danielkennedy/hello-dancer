d7d3b6035276948a0ea01fab1e23ba0eca675313
Dan Kennedy <dan@dankennedy.co>
2014-03-27 06:17:36 -0500

    Stuff

--- bin/app.pl
+++ bin/app.pl
@@ -3,5 +3,9 @@ use Dancer;
 use CFDemo::App;
 if (exists $ENV{VCAP_APP_PORT}) {
   set port => $ENV{VCAP_APP_PORT};
+} elsif (exists $ENV{PORT}) {
+  set port => $ENV{VCAP_APP_PORT};
+} else {
+  set port => 3333;
 }
 dance;
