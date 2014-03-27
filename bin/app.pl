3f7bac1d4d23ed016d09e10886eb48982a13cc7e
Dan Kennedy <dan@dankennedy.co>
2014-03-27 06:30:01 -0500

    Stuff

--- bin/app.pl
+++ bin/app.pl
@@ -1,6 +1,10 @@
 #!/usr/bin/env perl
 use Dancer;
 use CFDemo::App;
+print "Dancer ENV:\n";
+foreach my $k (keys(%ENV)) {
+  print "$k = $ENV{$k}\n";
+}
 if (exists $ENV{VCAP_APP_PORT}) {
   set port => $ENV{VCAP_APP_PORT};
 } elsif (exists $ENV{PORT}) {
